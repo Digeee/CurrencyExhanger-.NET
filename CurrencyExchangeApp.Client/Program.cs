6671092f9fa993469af76462ec36f319478833ce
Jeganathan Digevan <digee12@gmail.com>
2025-11-18 13:35:21 +0530

    currency exachange

--- CurrencyExchangeApp.Client/Program.cs
+++ CurrencyExchangeApp.Client/Program.cs
@@ -4,10 +4,10 @@ using CurrencyExchangeApp.Client.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 // Register services
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ExchangeRateApiService>();
 builder.Services.AddScoped<ICurrencyExchangeService>(sp => 
     new CachingCurrencyExchangeService(
         sp.GetRequiredService<ExchangeRateApiService>()));
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
--- CurrencyExchangeApp.Client/obj/Debug/net9.0/CurrencyExchangeApp.Client.AssemblyInfo.cs
+++ CurrencyExchangeApp.Client/obj/Debug/net9.0/CurrencyExchangeApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CurrencyExchangeApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e8962c050d50610e31ac25a3cafef2f5149ba65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30771e66b719cff5954ca361e6f81250c78ae81")]
 [assembly: System.Reflection.AssemblyProductAttribute("CurrencyExchangeApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CurrencyExchangeApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
